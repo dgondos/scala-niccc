296326ac235bedf92926142e4a00eee45aaea4d2
Daniel Gondos <dan.gondos@gmail.com>
2020-03-08 18:18:21 +1100

    refactor 1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -20,32 +20,24 @@ import scala.swing.event.MousePressed
 import scala.concurrent.Future
 import java.awt.Polygon
 
-object Hello extends SimpleSwingApplication {
+object NICCC extends SimpleSwingApplication {
     implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
     object ui extends Panel {
         background = Color.black
         preferredSize = new Dimension(255, 200)
-        var fg = Color.white
-        var polys = List[(List[Point], Color)]()
-
         focusable = true
         listenTo(mouse.clicks, mouse.moves, keys)
 
+        var fg = Color.white
+        var polys = List[(List[Point], Color)]()
+
         reactions += {
-            case e: MouseReleased => Future(startDraw())
+            case e: MouseReleased => Future(try { startDraw } catch { case e: Throwable => e.printStackTrace() } )
         }
 
-        /* records the dragging */
-
-        def drawPolys(p: List[(List[Point], Color)]): Unit = {
+        def updatePolys(p: List[(List[Point], Color)]): Unit = {
             polys = p
-            repaint()
-        }
-
-        def clear(): Unit = {
-            polys = List[(List[Point], Color)]()
-            repaint()
         }
 
         override def paintComponent(g: Graphics2D): Unit = {
@@ -73,38 +65,28 @@ object Hello extends SimpleSwingApplication {
         contents = ui
     }
 
-    val isBlank = (f: Int) =>  {
-        (f & (1L << 0)) != 0
-    }
-    val isPalette = (f: Int) => {
-        (f & (1L << 1)) != 0
-    }
-    val isIndexed = (f: Int) => {
-        (f & (1L << 2)) != 0
-    }
-    def readByte (it: Iterator[Byte]) : Int = {
-        it.next() & 0xFF // stupid java
-    }
-    def mkWord16eb (l: Int, r: Int) : Int = {
-        ((l << 8) | r)
-    }
-    def readDescriptor(d: Int) : (Int, Int) = {
-        ((d & 0xF0) >>> 4, d & 0xF)
-    }
-    def readPalette(it: Iterator[Byte], p: Array[Color]) : Array[Color] = {
-        val l = readByte(it)
-        val r = readByte(it)
-        val bitmask = mkWord16eb(l, r)
-        for (i <- 0 to 15) {
-            val set = (bitmask & (1L << i)) != 0
-            if (set) {
-                val cl = readByte(it)
-                val cr = readByte(it)
-                p.update(15 - i, decodeAtariSTColor(mkWord16eb(cl, cr)))
-            }
+    class FrameFlag(f: Int) {
+        def isBlank =  {
+            (f & (1L << 0)) != 0
         }
-        p
+        def isPalette = {
+            (f & (1L << 1)) != 0
+        }
+        def isIndexed = {
+            (f & (1L << 2)) != 0
+        }
     }
+
+    class PolyDescriptor(f: Int) {
+        def nbrVerts = {
+            f & 0xF
+        }
+        def colorIdx = {
+            (f & 0xF0) >>> 4
+        }
+        def value = f
+    }
+
     def decodeAtariSTColor(st: Int): Color = {
         //Atari ST format: 00000RRR0GGG0BBB
         val blue = (st & 0x7) << 4
@@ -112,88 +94,65 @@ object Hello extends SimpleSwingApplication {
         val red = ((st & 0x700) >>> 8) << 4
         new Color(red, green, blue)
     }
-    def readIndexedVerts(it: Iterator[Byte]) : List[(Int, Int)] = {
-        var verts = List[(Int, Int)]()
-
-        val nVerts = readByte(it)
-        for (i <- 0 until nVerts) {
-            val x = readByte(it)
-            val y = readByte(it)
-            verts = verts :+ (x, y)
-        }
-        verts
-    }
 
     val bis = new BufferedInputStream(new FileInputStream("scene1.bin"))
     val bin: Array[Byte] = Stream.continually(bis.read).takeWhile(-1 !=).map(_.toByte).toArray
 
-    def startDraw(): Unit = {
-        var frames = 0
-        var palette = Array[Color]()
+    def startDraw : Unit = {
+        var palette = Array.fill(16)(Color.black)
         for (b_i <- 0 until 9) {
             val block = bin.slice(b_i * 64 * 1024, (b_i + 1) * 64 * 1024)
             val it = block.iterator
+            def readByte : Int = {
+                it.next & 0xFF // stupid java
+            }
             var endOfBlock = false
 
-            while (!endOfBlock) { // no idea how many frames per block
-                val b_flag = readByte(it)
-                var indexedVerts = Array[(Int, Int)]()
+            while (!endOfBlock) { // variable frames per block
+                val flag = new FrameFlag(readByte)
+                if (flag.isPalette) {
+                    def mkWord16eb (l: Int, r: Int) : Int = {
+                        ((l << 8) | r)
+                    }
+                    val bitmask = mkWord16eb(readByte, readByte)
+                    for (i <- 0 to 15) {
+                        val set = (bitmask & (1L << i)) != 0
+                        if (set) {
+                            palette.update(15 - i, decodeAtariSTColor(mkWord16eb(readByte, readByte)))
+                        }
+                    }
+                }
+
+                val indexedVerts = if (flag.isIndexed) (for (i <- 0 until readByte) yield i).map(_ => new Point(readByte, readByte)).toArray else Array[Point]()
                 var polys = List[(List[Point], Color)]()
 
                 breakable {
-                    if (isBlank(b_flag)) {
-                        ui.clear()
-                    }
-                    if (isPalette(b_flag)) {
-                        for (i <- 0 until 16) {
-                            palette = palette :+ Color.black
-                        }
-                        palette = readPalette(it, palette)
-                    }
-                    if (isIndexed(b_flag)) {
-                        indexedVerts = indexedVerts ++ readIndexedVerts(it)
-                    }
-
-                    while (true) { // no idea a priori until when we have frame data
-                        val d = readByte(it)
-                        d match {
-                            case 0xFF => { // end of frame
-                                ui.drawPolys(polys)
+                    while (true) { // read until encountering special descriptor flag
+                        val d = new PolyDescriptor(readByte)
+                        d.value match {
+                            case 0xFF => {
+                                ui.updatePolys(polys)
+                                ui.repaint
                                 Thread.sleep(33)
-                                frames = frames + 1
                                 break
                             }
-                            case 0xFE => { // end of frame and block
+                            case 0xFE => {
                                 endOfBlock = true
-                                frames = frames + 1
                                 break
                             }
-                            case 0xFD => { // end of everything
+                            case 0xFD => {
                                 endOfBlock = true
-                                println("end of everything")
                                 break
                             }
                             case _ => { // data
-                                val (colorIdx, nbrVerts) = readDescriptor(d)
-                                val color = palette(colorIdx)
-                                var verts = List[Point]()
-                                for (i <- 0 until nbrVerts) {
-                                    if (isIndexed(b_flag)) {
-                                        val vert = indexedVerts(readByte(it))
-                                        verts = verts :+ new Point(vert._1, vert._2)
-                                    }
-                                    else {
-                                        verts = verts :+ new Point(readByte(it), readByte(it))
-                                    }
-                                }
-                                polys = polys :+ (verts, color)
+                                val verts = (for (i <- 0 until d.nbrVerts) yield i).map(_ => if (flag.isIndexed) indexedVerts(readByte) else new Point(readByte, readByte)).toList
+                                polys = polys :+ (verts, palette(d.colorIdx))
                             }
                         }
                     }
                 }
             }
         }
-        println(s"Nbr frames: ${frames}")
     }
 
 }
